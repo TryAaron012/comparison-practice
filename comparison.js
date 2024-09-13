226ce663b5b1d3e6370b1ebef73e79c8cd5b58b3
Aaron Raymund Paguirigan <paguiriganaaronraymund@gmail.com>
2024-09-13 15:26:39 +0800

    initial commit

---
+++ comparison.js
@@ -0,0 +1,17 @@
+let firstFavNumb = 7;
+let secondFavNumb = 15; 
+
+
+console.log("Is Greater than:", firstFavNumb > secondFavNumb); 
+console.log("Less than:", firstFavNumb < secondFavNumb);
+console.log("Greater than or equal to:", firstFavNumb >= secondFavNumb);
+console.log("Less than or equal to:", firstFavNumb <= secondFavNumb);
+console.log("Strict equality (===):", firstFavNumb === secondFavNumb);
+console.log("Loose equality (==):", firstFavNumb == secondFavNumb);
+console.log("Strict inequality (!==):", firstFavNumb !== secondFavNumb);
+console.log("Loose inequality (!=):", firstFavNumb != secondFavNumb); 
+
+
+console.log("AND:", firstFavNumb > 7 && secondFavNumb > 15);
+console.log("OR :", firstFavNumb > 7 || secondFavNumb < 15); 
+console.log("NOT (!):", !(firstFavNumb < 7)); 
